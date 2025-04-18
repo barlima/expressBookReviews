36fe89f94637b54e76752d9d18e6eec21fc4c8d3
Bartlomiej Perucki <bartlomiejperucki@Bartlomiejs-MacBook-Pro-2.local>
2025-04-18 14:21:05 +0200

    Task 4

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -42,8 +42,17 @@ public_users.get("/author/:author", function (req, res) {
 
 // Get all books based on title
 public_users.get("/title/:title", function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const title = req.params.title;
+
+  const booksByTitle = Object.values(books).filter(
+    (book) => book.title === title
+  );
+
+  if (booksByTitle.length === 0) {
+    return res.status(404).json({ message: "No books found for this title" });
+  }
+
+  return res.json(booksByTitle);
 });
 
 //  Get book review
